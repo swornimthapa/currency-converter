d7b0f4a407ac86724a559c308f8b056f187a5fe3
swornimthapa <swornimtheking@gmail.com>
2024-01-08 16:31:23 +0545

    exchange rates done

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 const flagBaseUrl = "https://flagsapi.com/BE/flat/64.png";
 const selectAll = document.querySelectorAll("select");
-// const flagImage = document.querySelectorAll(".select-container img");
-// console.log(flagImage);
-// let previousElementSibling;
+const button = document.querySelector(".button");
+const fromCurrency = document.querySelector(".from-container select");
+const toCurrency = document.querySelector(".to-container select");
+let showResult = document.querySelector("#exchageRate");
+
 for(let select of selectAll){
-    // console.log(select);
+    
     for(let currencyCode in countryList){
         let newOption = document.createElement("option");
         newOption.value = currencyCode;
         newOption.textContent = currencyCode;
         select.appendChild(newOption);
         if(select.id == "from-select"){
-            select.value = "NPR";
-        }else if(select.id == "to-select"){
             select.value = "USD";
+        }else if(select.id == "to-select"){
+            select.value = "NPR";
         }
     }
     select.addEventListener("change",(event)=>{
-        // console.log(event.target);
+        
         updateFlag(event.target,select);
     })
 }
@@ -26,6 +28,30 @@ for(let select of selectAll){
 function updateFlag(option,select){
     let image = select.previousElementSibling;
     console.log(image);
+    
     console.log(countryList[option.value]);
     image.src = `https://flagsapi.com/${countryList[option.value]}/flat/64.png`;
 }
+
+
+async function getRates(){
+    let amount =Number(document.getElementById("amount").value);
+    let from = fromCurrency.value.toLowerCase();
+    let to = toCurrency.value.toLowerCase();
+    // console.log(form,to , amount);
+    let response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${to}.json`)
+    let data = await response.json();
+    let rate = data[to];
+    let date = (data["date"]);
+    showResult.innerHTML= `${amount} ${from.toUpperCase()} = ${(rate * amount).toFixed(2)} ${to.toUpperCase()} &nbsp;&nbsp;  (${date})`;
+
+}
+
+
+button.addEventListener("click",async ()=>{
+    getRates();
+
+});
+window.addEventListener("load", () => {
+    getRates();
+  });
